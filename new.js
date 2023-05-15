b16b31f9adf707c5a45919c3e01e82fa65fd3934
swadheeffainus <mohammedswadheef@gmail.com>
2023-05-15 10:00:12 +0100

    intial.

--- new.js
+++ new.js
@@ -1,6 +1,12 @@
 function scroll(){
-    let position = 1098
+    let position = 694
    window.scrollTo({
     top : position,
    })
 }
+
+
+// function newscrol(){
+//     let v = document.documentElement.scrollTop || document.body.scrollTop
+//     console.log(v)
+// }
