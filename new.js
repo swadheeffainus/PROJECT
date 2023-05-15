962be985b6ca006b4feb0995060c478d09a2e645
swadheeffainus <mohammedswadheef@gmail.com>
2023-05-15 14:57:30 +0100

    intial.

--- new.js
+++ new.js
@@ -8,5 +8,12 @@ function scroll(){
 
 // function newscrol(){
 //     let v = document.documentElement.scrollTop || document.body.scrollTop
-//     console.log(v)
+//     // console.log(v)
+
+//     let footer = document.getElementById("footer")
+//     if(v > 3020){
+//         footer.style.height = "600px"
+//     }else{
+//         footer.style.height = ""
+//     }
 // }
